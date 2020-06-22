558a97ba8f00c6f414785ba18613959a0dde33ba
loicbourgois <lbourgois@scaleway.com>
2020-06-22 17:08:38 +0200

    start work on mnist

---
+++ mnist/index.js
@@ -0,0 +1,8 @@
+const base_url = ''
+
+const dataset = 'mnist_png'
+const labels = `${dataset}/labels.txt`
+
+//fetch(labels)
+//  .then(response => response.text())
+//  .then(text => console.log(text))
