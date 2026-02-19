9beeb0f83a1ca2f5b62a4d9dda73546f98be44e5
Loïc Bourgois <loic@bourgois.pro>
2026-02-19 23:06:52 +0100

    up

--- movie-finder/index.js
+++ movie-finder/index.js
@@ -1,4 +1,4 @@
-import { 
+import {
     get_config
  } from "./config.js"
 import { 
@@ -9,10 +9,6 @@ import {
 const main = async () => {
     const config = get_config(window.location.hostname)
     const r = await get(`https://${config.server}/about`)
-    // const r = await get(`http://api.loicbourgois.com:3000/about`)
-    // const r = await get(`https://localhost:3000/about`)
-    // const r2 = await get(`https://127.0.0.1:3000/about`)
-    // const r2 = await get(`https://api.loicbourgois.com:3000/about`)
     console.log(r)
 }
 main()
