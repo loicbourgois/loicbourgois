22a58321cafc511de69348a9a9eff0fd536ee343
Loïc Bourgois <loic@bourgois.pro>
2026-02-21 19:20:56 +0100

    up

--- movie-finder/etl/pull.sh
+++ movie-finder/etl/pull.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 cd $HOME/github.com/loicbourgois/loicbourgois/movie-finder
-$HOME/github.com/loicbourgois/loicbourgois/movie-finder/etl/.venv/bin/python3.14 \
+$HOME/github.com/loicbourgois/loicbourgois/movie-finder/etl/.venv/bin/python3 \
     -m etl.pull
