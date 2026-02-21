09910bfcb188d0015189c3f97cd1eb7ebb41ea04
Loïc Bourgois <loic@bourgois.pro>
2026-02-21 19:18:32 +0100

    up

--- movie-finder/etl/setup.sh
+++ movie-finder/etl/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 cd $HOME/github.com/loicbourgois/loicbourgois/movie-finder/etl
-python -m venv .venv
+python3 -m venv .venv
 $HOME/github.com/loicbourgois/loicbourgois/movie-finder/etl/.venv/bin/python3.14 \
     -m pip install --upgrade pip
 $HOME/github.com/loicbourgois/loicbourgois/movie-finder/etl/.venv/bin/python3.14 \
