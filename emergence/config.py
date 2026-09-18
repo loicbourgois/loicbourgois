ce38bf3231a111738305a5dd269f4ff8d84be4df
Loïc Bourgois <loic@bourgois.pro>
2026-09-19 00:54:15 +0200

    up

---
+++ emergence/config.py
@@ -0,0 +1,13 @@
+preferences = {
+    "hydration": ["thirsty", "hydrated"],
+    "nutrition": ["hungry", "full"],
+    "environment": ["nature", "urban"],
+    "time": ["night", "day"],
+    "visibility": ["obscurity", "light"],
+    "temperature": ["cold", "heat"],
+}
+
+effort = distance_traveled + thinking
+food_eaten = ...
+hydration = hydration - external_temperature - effort
+nutrition = nutrition - effort + food_eaten
